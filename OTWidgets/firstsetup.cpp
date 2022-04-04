dd634f2802dda017e15b57e6a50996f10c3bc95f
pietr26 <96007573+pietr26@users.noreply.github.com>
2022-04-04 18:15:46 +0200

    Restyled startup screen

---
+++ OTWidgets/firstsetup.cpp
@@ -0,0 +1,34 @@
+#include "firstsetup.h"
+#include "ui_firstsetup.h"
+
+firstSetup::firstSetup(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::firstSetup)
+{
+    ui->setupUi(this);
+
+    wSettings wSet;
+    // Very simple: Copy from wsettings.cpp, wSettings::wSettings():
+    ui->cobxLanguage->addItem(QIcon(":/rec/data/flags/en.svg"), set.langEn);
+    ui->cobxLanguage->addItem(QIcon(":/rec/data/flags/de.svg"), set.langDe);
+    ui->cobxLanguage->addItem(QIcon(":/rec/data/flags/fr.svg"), set.langFr);
+    ui->cobxLanguage->addItem(QIcon(":/rec/data/flags/it.svg"), set.langIt);
+    ui->cobxLanguage->addItem(QIcon(":/rec/data/flags/cz.svg"), set.langCz);
+
+    // Disable 'it'
+    auto* model3 = qobject_cast<QStandardItemModel*>(ui->cobxLanguage->model());
+    model3->item(3)->setEnabled(false);
+}
+
+firstSetup::~firstSetup()
+{
+    delete ui;
+}
+
+
+void firstSetup::on_btnContinue_clicked()
+{
+    set.write("main", "language", ui->cobxLanguage->currentIndex());
+    misc.restart();
+}
+
