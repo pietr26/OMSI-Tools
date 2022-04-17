2ce3295cfebd9a2c1ad0f7d4d4e944def4a2162a
pietr26 <96007573+pietr26@users.noreply.github.com>
2022-04-18 01:42:41 +0200

    OTStyleDraw: Refactor

--- OTBackend/OTStyleDraw.h
+++ OTBackend/OTStyleDraw.h
@@ -5,11 +5,14 @@
 #include <QStyle>
 #include <QStyleOption>
 
+/// Class for style fixes
 class OTStyleDraw : public QProxyStyle
 {
 public:
+    /// [OVERRIDE]
     void drawPrimitive(PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
     {
+        // If the element is the frame of statusbar items, do nothing (removes the line behind each item)
         if (element == QStyle::PE_FrameStatusBarItem)
             return;
 
