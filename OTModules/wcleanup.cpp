bf341d55c119174fe4163e697ed1acb608741a38
pietr26 <96007573+pietr26@users.noreply.github.com>
2022-06-05 16:56:05 +0200

    Beta version of cleanup module

--- OTModules/wcleanup.cpp
+++ OTModules/wcleanup.cpp
@@ -24,25 +24,34 @@ wCleanup::~wCleanup()
     delete ui;
 }
 
-/* NEEDS:
- * getTiles();
- *  --> getItems();
- */
-
 void wCleanup::on_btnAnalyze_clicked()
 {
+    qInfo() << "Starting...";
+    ui->statusbar->showMessage(tr("Initialize..."));
+    ui->pgbProgress->setValue(1);
+    ui->btnAnalyze->setEnabled(false);
+
+    ui->lwgObjects->clear();
+    ui->lwgSplines->clear();
+
     QStringList globals;
 
     QDirIterator mapFolder(set.read("main", "mainDir").toString() + "/maps", QStringList() << "global.cfg", QDir::Files, QDirIterator::Subdirectories);
     while (mapFolder.hasNext())
         globals << mapFolder.next();
 
+    qInfo() << "Read maps...";
+
+    ui->pgbProgress->setMaximum(ui->pgbProgress->maximum() + globals.count());
+    int i = 0;
     foreach (QString current, globals)
     {
+        i++;
+        ui->statusbar->showMessage(tr("Read maps (%1 of %2)...").arg(i).arg(globals.count()));
+        ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+
         filehandler.setMapPath(current);
-
         filehandler.getTiles();
-
         filehandler.getItems(filehandler.stuffobj.existing.tiles);
     }
 
@@ -50,13 +59,20 @@ void wCleanup::on_btnAnalyze_clicked()
     filehandler.stuffobj.existing.splines.removeDuplicates();
 
     // Sceneryobjects
+    qInfo() << "Analyze sceneryobjects...";
+    ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+    ui->statusbar->showMessage(tr("Analyze sceneryobject folder..."));
+
     QStringList scoFolders;
     QDirIterator scoFolder(set.read("main", "mainDir").toString() + "/Sceneryobjects", QDir::Dirs | QDir::NoDotAndDotDot);
     while (scoFolder.hasNext())
         scoFolders << scoFolder.next();
 
+    ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+    ui->statusbar->showMessage(tr("Comparing sceneryobjects..."));
     foreach (QString current, scoFolders)
     {
+        qApp->processEvents();
         QStringListIterator iterator(filehandler.stuffobj.existing.sceneryobjects);
 
         int i = 0;
@@ -69,13 +85,20 @@ void wCleanup::on_btnAnalyze_clicked()
     }
 
     // Splines:
+    qInfo() << "Analyze splines...";
+    ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+    ui->statusbar->showMessage(tr("Analyze spline folder..."));
+
     QStringList sliFolders;
     QDirIterator sliFolder(set.read("main", "mainDir").toString() + "/Splines", QDir::Dirs | QDir::NoDotAndDotDot);
     while (sliFolder.hasNext())
         sliFolders << sliFolder.next();
 
+    ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+    ui->statusbar->showMessage(tr("Comparing splines..."));
     foreach (QString current, sliFolders)
     {
+        qApp->processEvents();
         QStringListIterator iterator(filehandler.stuffobj.existing.splines);
 
         int i = 0;
@@ -87,6 +110,10 @@ void wCleanup::on_btnAnalyze_clicked()
             ui->lwgSplines->addItem(current);
     }
 
-    qInfo() << "+++ Finished.";
+    ui->btnAnalyze->setEnabled(true);
+    ui->pgbProgress->setValue(ui->pgbProgress->value() + 1);
+    ui->statusbar->showMessage(tr("Done."), 10000);
+
+    qInfo() << "Finished.";
 }
 
