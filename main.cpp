e2ec8cc121c23d9bb10aeaee97e35002cc1618c3
pietr26 <96007573+pietr26@users.noreply.github.com>
2023-01-14 17:54:32 +0100

    Clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ void updateCleanup()
     // Necessary for every update:
     QDir(QDir::tempPath() + "/OMSI-Tools_tempAppDir").removeRecursively();
 
-    // Necessary for specified versionsf:
+    // Necessary for specified versions:
 
     // 001: >  1.0.0-lite - Remove unused image libaries
     // Moved to Updater (permission conflicts)
@@ -60,60 +60,62 @@ int main(int argc, char *argv[])
     OTMiscellaneous misc;
     OTFileOperations fop;
 
-    // If application crashed, save logfile
-    QString newName;
-    bool isCrash = false;
-    if (set.read("main", "closeCheck") == "false")
-    {
-        isCrash = true;
-        newName = QString("logfile_crash_%1.txt").arg(misc.getDate("yyyy-MM-dd") + "_" + misc.getTime("hh-mm-ss"));
-
-        QFile::copy("logfile.txt", newName);
-    }
-
-    // Initialize logger
-    if ((set.read("main", "logfileMode") != -1))
-    {
-        Logger::attach();
-        Logger::logging = true;
-    }
-
-    // Print all settings to the logfile
-    qDebug().noquote() << "Settings" + set.getAllSettings() + "\n================================================\n";
-
-    set.write("main", "closeCheck", false);
-    set.setDefaultSettings();
-
-    qInfo().noquote() << "Starting " + OTInformation::name + "...";
-
     QApplication a(argc, argv);
 
-    QApplication::setStyle(new OTStyleDraw);
-
-    set.loadTranslator();
-
-    if (isCrash)
+    /// Setup
     {
-        qWarning() << "Crash on last start detected! A logfile has been saved. Please contact the developer.";
-        QMessageBox::StandardButton reply = QMessageBox::question(NULL, QObject::tr("Crash detected"), QObject::tr("%1 seems to have crashed on last launch. The logfile of the last start was saved separately. Please contact the developer with this.\nOpen the path of the logfile?").arg(OTInformation::name));
+        // If application crashed, save logfile
+        QString newName;
+        bool isCrash = false;
+        if (set.read("main", "closeCheck") == "false")
+        {
+            isCrash = true;
+            newName = QString("logfile_crash_%1.txt").arg(misc.getDate("yyyy-MM-dd") + "_" + misc.getTime("hh-mm-ss"));
 
-        if (reply == QMessageBox::Yes)
-            fop.showInExplorer(newName);
+            QFile::copy("logfile.txt", newName);
+        }
+
+        // Initialize logger
+        if ((set.read("main", "logfileMode") != -1))
+        {
+            Logger::attach();
+            Logger::logging = true;
+        }
+
+        // Print all settings to the logfile
+        qDebug().noquote() << "Settings" + set.getAllSettings() + "\n================================================\n";
+
+        set.write("main", "closeCheck", false);
+        set.setDefaultSettings();
+
+        qInfo().noquote() << "Starting " + OTInformation::name + "...";
+
+        QApplication::setStyle(new OTStyleDraw);
+        set.loadTranslator();
+
+        if (isCrash)
+        {
+            qWarning() << "Crash on last start detected! A logfile has been saved. Please contact the developer.";
+            QMessageBox::StandardButton reply = QMessageBox::question(NULL, QObject::tr("Crash detected"), QObject::tr("%1 seems to have crashed on last launch. The logfile of the last start was saved separately. Please contact the developer with this.\nOpen the path of the logfile?").arg(OTInformation::name));
+
+            if (reply == QMessageBox::Yes)
+                fop.showInExplorer(newName);
+        }
+        else
+            qDebug() << "No crash detected.";
+
+        testArea();
+
+        // If the application was updated, remove the temporary directory in the temp folder
+        if ((QCoreApplication::arguments().size() >= 2) && (QCoreApplication::arguments().at(1) == "updateInstalled"))
+            updateCleanup();
+
+        // Start counter
+        if (set.read("main", "startCount").isValid())
+            set.write("main", "startCount", set.read("main", "startCount").toInt() + 1);
+        else
+            set.write("main", "startCount", 1);
     }
-    else
-        qDebug() << "No crash detected.";
-
-    testArea();
-
-    // If the application was updated, remove the temporary directory in the temp folder
-    if ((QCoreApplication::arguments().size() >= 2) && (QCoreApplication::arguments().at(1) == "updateInstalled"))
-        updateCleanup();
-
-    // Start counter
-    if (set.read("main", "startCount").isValid())
-        set.write("main", "startCount", set.read("main", "startCount").toInt() + 1);
-    else
-        set.write("main", "startCount", 1);
 
     wStartUpScreen *WSTARTUPSCREEN;
     wFbh *WFBH;
