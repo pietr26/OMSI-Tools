a4292567a8193e9d975f2bf14ef644546d45c38f
pietr26 <96007573+pietr26@users.noreply.github.com>
2023-04-11 17:56:28 +0200

    Added wMaps module

---
+++ OTModules/wMaps/wmaps.h
@@ -0,0 +1,51 @@
+#ifndef WMAPS_H
+#define WMAPS_H
+
+#include <QMainWindow>
+#include "OTBackend/OTGlobal.h"
+#include "OTBackend/OTOmsiFileHandler.h"
+#include "OTModules/OTGeneric/wsettings.h"
+#include "OTModules/OTGeneric/wfeedback.h"
+
+namespace Ui {
+class wMaps;
+}
+
+class wMaps : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit wMaps(QWidget *parent = nullptr);
+    ~wMaps();
+
+signals:
+    void backToHome();
+
+private slots:
+    void on_actionSettings_triggered();
+
+    void on_actionBackToHome_triggered();
+
+    void on_actionClose_triggered();
+
+    void on_actionSendFeedback_triggered();
+
+    void on_cobxMapName_currentIndexChanged(int index);
+
+private:
+    Ui::wMaps *ui;
+
+    OTMessage msg;
+    OTSettings set;
+    OTMiscellaneous misc;
+    OTOMSIFileHandler filehandler;
+    wSettings *WSETTINGS;
+
+    QList<QPair<QString, QString>> mapList;
+
+    void loadMapList();
+    bool mapListSetupFinished = false;
+};
+
+#endif // WMAPS_H
