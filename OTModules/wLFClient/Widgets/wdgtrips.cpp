9b6e6e415ab93d97a098de06f68152547237c920
pietr26 <96007573+pietr26@users.noreply.github.com>
2024-06-04 23:33:51 +0200

    Added base functionality of wdgTrips

--- OTModules/wLFClient/Widgets/wdgtrips.cpp
+++ OTModules/wLFClient/Widgets/wdgtrips.cpp
@@ -1,41 +1,72 @@
 #include "wdgtrips.h"
 #include "ui_wdgtrips.h"
 
-wdgTrips::wdgTrips(QWidget *parent)
+wdgTrips::wdgTrips(QWidget *parent, LFClientAPIInterface *api)
     : QWidget(parent)
-    , ui(new Ui::wdgTrips)
+    , ui(new Ui::wdgTrips),
+    api(api)
 {
     ui->setupUi(this);
 
-    trips = new QStandardItemModel(3, 3);
-    trips->setHeaderData(0, Qt::Horizontal, tr("Code"));
-    trips->setHeaderData(1, Qt::Horizontal, tr("From"));
-    trips->setHeaderData(2, Qt::Horizontal, tr("To"));
+    timer10s = new QTimer(this);
+    connect(timer10s, &QTimer::timeout, this, &wdgTrips::reloadUi10s);
+    timer10s->start(10000);
 
-    trips->setData(trips->index(0, 0), "76/1-1");
-    trips->setData(trips->index(0, 1), "[04:07] Nordspitze");
-    trips->setData(trips->index(0, 2), "[04:??] Bauernhof");
-
-    trips->setData(trips->index(1, 0), "76/1-1");
-    trips->setData(trips->index(1, 1), "[04:22] Bauernhof");
-    trips->setData(trips->index(1, 2), "[04:??] Nordspitze");
-
-    trips->setData(trips->index(2, 0), "76/1-1");
-    trips->setData(trips->index(2, 1), "[04:37] Nordspitze");
-    trips->setData(trips->index(2, 2), "[04:??] Bauernhof");
-
-    QFont font;
-    font.setStrikeOut(true);
-    font.setItalic(true);
-    trips->item(2, 0)->setFont(font);
-    trips->item(2, 1)->setFont(font);
-    trips->item(2, 2)->setFont(font);
-
-    ui->tvwTrips->setModel(trips);
-    ui->tvwTrips->resizeColumnsToContents();
+    reloadUi10s();
+    connect(api, &LFClientAPIInterface::loginStatusChanged, this, &wdgTrips::reloadUi10s);
 }
 
 wdgTrips::~wdgTrips()
 {
     delete ui;
+}
+
+void wdgTrips::on_btnTest_clicked()
+{
+    if (api->isLoggedIn())
+    {
+        LFCApiSpeakRequest test = api->getMySpeakRequest();
+        qInfo() << "test";
+    }
+}
+
+void wdgTrips::reloadUi10s()
+{
+    if (api->isLoggedIn())
+    {
+        QList<LFCApiTrip> tripData = api->getMyTrips();
+
+        trips = new QStandardItemModel(tripData.count(), 6);
+        trips->setHeaderData(0, Qt::Horizontal, tr("Vehicle"));
+        trips->setHeaderData(1, Qt::Horizontal, tr("Line"));
+        trips->setHeaderData(2, Qt::Horizontal, tr("Route"));
+        trips->setHeaderData(3, Qt::Horizontal, tr("Trip"));
+        trips->setHeaderData(4, Qt::Horizontal, tr("From"));
+        trips->setHeaderData(5, Qt::Horizontal, tr("To"));
+
+        for (int i = 0; i < tripData.count(); i++)
+        {
+            trips->setData(trips->index(i, 0), api->getVehicle(tripData[i].vehicle()).number());
+            trips->setData(trips->index(i, 1), tripData[i].line());
+            trips->setData(trips->index(i, 2), tripData[i].route());
+            trips->setData(trips->index(i, 3), tripData[i].name());
+            trips->setData(trips->index(i, 4), (tripData[i].busstops().isEmpty() ? "---" : tripData[i].busstops().first().name() + " (" + tripData[i].busstops().first().time().toString("HH:mm") + ")"));
+            trips->setData(trips->index(i, 5), (tripData[i].busstops().isEmpty() ? "---" : tripData[i].busstops().last().name() + " (" + tripData[i].busstops().last().time().toString("HH:mm") + ")"));
+        }
+
+        // QFont font;
+        // font.setStrikeOut(true);
+        // font.setItalic(true);
+        // trips->item(2, 0)->setFont(font);
+        // trips->item(2, 1)->setFont(font);
+        // trips->item(2, 2)->setFont(font);
+
+        ui->tvwTrips->setModel(trips);
+        ui->tvwTrips->resizeColumnsToContents();
+    }
+    else
+    {
+        trips = new QStandardItemModel();
+        ui->tvwTrips->setModel(trips);
+    }
 }
--- OTModules/wLFClient/Widgets/wdgtrips.h
+++ OTModules/wLFClient/Widgets/wdgtrips.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QStandardItemModel>
+#include "OTBackend/LFClientAPIInterface/lfclientapiinterface.h"
+#include <QTimer>
 
 namespace Ui {
 class wdgTrips;
@@ -13,13 +15,23 @@ class wdgTrips : public QWidget
     Q_OBJECT
 
 public:
-    explicit wdgTrips(QWidget *parent = nullptr);
+    explicit wdgTrips(QWidget *parent = nullptr, LFClientAPIInterface *api = nullptr);
     ~wdgTrips();
+
+private slots:
+    void on_btnTest_clicked();
 
 private:
     Ui::wdgTrips *ui;
 
     QStandardItemModel *trips;
+
+    LFClientAPIInterface *api;
+
+    QTimer *timer10s;
+
+private slots:
+    void reloadUi10s();
 };
 
 #endif // WDGTRIPS_H
