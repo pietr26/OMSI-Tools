117213226cc7044643c779802622a0866383ace0
pietr26 <96007573+pietr26@users.noreply.github.com>
2024-11-09 21:15:39 +0100

    Updated translation scripts to Qt 6.8.0 and minggw1310

--- data/translations/2 lrelease-pro.bat
+++ data/translations/2 lrelease-pro.bat
@@ -2,9 +2,9 @@
 echo Setting up environment...
 echo.
 echo.
-set PATH=C:\Qt\6.7.2\mingw_64\bin;C:/Qt/Tools/mingw1120_64/bin;%PATH%
+set PATH=C:\Qt\6.8.0\mingw_64\bin;C:/Qt/Tools/mingw1310_64/bin;%PATH%
 
-cd C:\Qt\6.7.2\mingw_64\bin
+cd C:\Qt\6.8.0\mingw_64\bin
 lrelease-pro.exe D:\OMSI-Tools\OMSI-Tools\OMSI-Tools.pro
 
 echo ------------------------------------------------------------------------------------------
